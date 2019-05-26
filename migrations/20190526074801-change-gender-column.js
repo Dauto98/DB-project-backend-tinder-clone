4bc19e069c8c3636ac58dc947189ae5975c28cdf
Dauto98 <dauto98@gmail.com>
2019-05-26 16:24:31 +0700

    add migration and seeder

---
+++ migrations/20190526074801-change-gender-column.js
@@ -0,0 +1,22 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.removeColumn("User", "gender").then(() => queryInterface.addColumn("User", "gender", Sequelize.ENUM("F", "M")));
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
--- seeders/20190525145515-demo-user.js
+++ seeders/20190525145515-demo-user.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
@@ -10,7 +13,25 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-
+    return new Promise((resolve, reject) => {
+      fs.readFile(path.resolve(__dirname, "./sample-data.json"), (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        const userData = JSON.parse(data);
+        userData.forEach(user => {
+          user.dob = Sequelize.fn("to_timestamp", user.dob);
+          user.createdAt = Sequelize.fn("now");
+          user.updatedAt = Sequelize.fn("now");
+        });
+        return queryInterface.bulkInsert("User", userData).then(() => {
+          resolve();
+        }).catch(err => {
+          console.error(err);
+          reject(err);
+        });
+      });
+    });
   },
 
   down: (queryInterface, Sequelize) => {
