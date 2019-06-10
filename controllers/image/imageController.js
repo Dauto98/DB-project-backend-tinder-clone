9f2ce6440a94bebf58d3e25c7ea5f1c53d58a5fe
Dauto98 <dauto98@gmail.com>
2019-06-10 22:05:05 +0700

    fix image order

--- controllers/image/imageController.js
+++ controllers/image/imageController.js
@@ -4,25 +4,37 @@ const db = require("../../models");
 
 module.exports = {
   getImageOfUser: (req, res) => {
-    db.Image.findAll({ where: { userId: req.params.id } }).then(imageData => {
-      if (imageData.length) {
-        const imageUrls = imageData.map((image, index) => ({ id: image.id, url: cloudinary.url(image.id), order: image.order || index })).sort((a, b) => a.order - b.order);
-        res.json(imageUrls);
-      } else {
-        res.json([]);
-      }
+    db.Image.destroy({
+      where: { order: null }
+    }).then(() => {
+      db.Image.findAll({ where: { userId: req.params.id } }).then(imageData => {
+        if (imageData.length) {
+          const imageUrls = imageData.map((image) => ({ id: image.id, url: cloudinary.url(image.id), order: image.order })).sort((a, b) => a.order - b.order);
+          res.json(imageUrls);
+        } else {
+          res.json([]);
+        }
+      });
     });
   },
 
   insert: (req, res) => {
     if (req.body.image) {
-      const imageId = uuid();
-      db.Image.create({
-        id: imageId,
+      db.Image.findOne({ where: {
         userId: req.userData.userId,
         order: req.body.imgOrder
-      }).then(() => {
-        cloudinary.uploader.upload(req.body.image, { public_id: imageId }, (err, result) => {
+      } }).then(oldImage => {
+        if (!oldImage) {
+          const imageId = uuid();
+          return db.Image.create({
+            id: imageId,
+            userId: req.userData.userId,
+            order: req.body.imgOrder
+          });
+        }
+        return Promise.resolve(oldImage);
+      }).then((image) => {
+        cloudinary.uploader.upload(req.body.image, { public_id: image.id, invalidate: true }, (err, result) => {
           if (err) {
             console.log(err);
             res.status(500).json({ message: "Something is wrong with image storage" });
