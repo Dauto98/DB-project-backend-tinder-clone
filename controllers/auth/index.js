f3f14a87c5046734a9cd78fea5a75150a2cdf741
Dauto98 <dauto98@gmail.com>
2019-06-18 13:24:30 +0700

    remove password constraint

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.post("/login", check("email").isEmail(), controller.login);
 
 router.post("/register", check("email").isEmail(),
-  check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long")
-    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+  // check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long")
+  //   .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
   controller.register);
 
 module.exports = router;
